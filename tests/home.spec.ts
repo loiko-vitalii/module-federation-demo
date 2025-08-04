6e8ddb624cc1cdd5c6c36617dc27aa4ff6c757f2
vitaliiloyko <vitaliy@icloud.com>
2025-08-04 13:16:53 +0300

    fix(app): change page title

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -3,7 +3,7 @@ import { test, expect } from '@playwright/test';
 
 test.describe('Home Page', () => {
   test.beforeEach(async ({ page }) => {
-    await page.goto('http://localhost:3000', {waitUntil: 'networkidle'});
+    await page.goto('http://localhost:3000', { waitUntil: 'domcontentloaded' });
   });
 
   test('should display correct page title', async ({ page }) => {
