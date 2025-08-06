c4cef42bc3c750823133c392203ec756f5fb0b41
vitaliiloyko <vitaliy@icloud.com>
2025-08-06 22:30:16 +0300

    fix(app): change page title

--- playwright.config.ts
+++ playwright.config.ts
@@ -78,6 +78,7 @@ export default defineConfig({
     command: 'pnpm run start',
     url: 'http://localhost:3000',
     // reuseExistingServer: !process.env.CI,
+    reuseExistingServer: true,
     timeout: 120_000,
   },
 });
