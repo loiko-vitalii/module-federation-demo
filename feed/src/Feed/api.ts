fabe2695552a67854547e53564ae61fdf73e49cc
vitaliiloyko <vitaliy@icloud.com>
2025-08-08 00:38:47 +0300

    fix(app): change page title

--- feed/src/Feed/api.ts
+++ feed/src/Feed/api.ts
@@ -1,7 +1,6 @@
 const YOUTUBE_API_BASE = 'https://www.googleapis.com/youtube/v3';
 const YOUTUBE_DATA_API_KEY = import.meta.env.PUBLIC_YOUTUBE_DATA_API_KEY;
 
-console.log('YOUTUBE_DATA_API_KEY', YOUTUBE_DATA_API_KEY);
 const cache = new Map();
 const createCacheKey = (key: unknown[]) => JSON.stringify(key);
 
