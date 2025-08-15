7f8cdc3df0d295a32368dfac41685ab05f2a937e
vitaliiloyko <vitaliy@icloud.com>
2025-08-15 21:18:29 +0300

    chore(uikit): enable source maps

--- uikit/rsbuild.config.ts
+++ uikit/rsbuild.config.ts
@@ -16,6 +16,9 @@ export default defineConfig({
     cssModules: {
       namedExport: true,
     },
+    sourceMap: {
+      js: 'source-map',
+    },
     assetPrefix: `${DOMAIN}/uikit/${VERSION}`,
   },
 });
