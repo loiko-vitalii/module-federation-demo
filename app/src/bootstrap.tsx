fc43cfd5a366778186e8246d2a27a723fc6df719
vitaliiloyko <vitaliy@icloud.com>
2025-08-15 20:24:59 +0300

    chore(logging): setup sentry

--- app/src/bootstrap.tsx
+++ app/src/bootstrap.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 
+import * as Sentry from '@sentry/react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import { App } from './App';
 import './global.module.css';
+
+Sentry.init({
+  dsn: 'https://59bf180109528bceeffada28adab6a0a@o4509849200623616.ingest.de.sentry.io/4509849238896720',
+  // Setting this option to true will send default PII data to Sentry.
+  // For example, automatic IP address collection on events
+  sendDefaultPii: true,
+});
 
 const rootEl = document.getElementById('root');
 
@@ -15,6 +23,14 @@ if (rootEl) {
     <React.StrictMode>
       <BrowserRouter>
         <App />
+        <button
+          type="button"
+          onClick={() => {
+            throw new Error('Error');
+          }}
+        >
+          Error Button
+        </button>
       </BrowserRouter>
     </React.StrictMode>,
   );
